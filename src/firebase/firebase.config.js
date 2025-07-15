ddffd846e30a1e1dacd2c56adc2752d4e0324c0f
Rasheed893 <106796468+Rasheed893@users.noreply.github.com>
2025-07-15 15:45:08 +0400

    firebase test

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -18,6 +18,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+console.log("App ID:", import.meta.env.VITE_APP_ID);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
